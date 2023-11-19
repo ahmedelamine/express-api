7485bb810038fd363ce75b38b3507ef27e072fa1
ahmedelamine <42996518+ahmedelamine@users.noreply.github.com>
2023-11-19 11:04:45 +0100

    new route

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get("/test", (req, res) => {
+    res.send("<h1>Testing work</h1>")
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
